386defcf079a1084aacc69b15482f48fd6fd3f33
Moisés <olimpiodev.tec@gmail.com>
2026-06-20 15:51:35 -0300

    Testando App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { View, Text } from 'react-native';
+
+export default function App() {
+
+  return (
+
+    <View>
+      <Text >Hello World React Native com Expo Go!</Text>
+    </View>
+    
+  );
+}
+
